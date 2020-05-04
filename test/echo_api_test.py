424606c59737ebc327ae2f16203fe740b50f2f5d
begert <begi@gmx.ch>
2020-05-04 17:54:08 +0200

    extended api_tests for github

--- test/echo_api_test.py
+++ test/echo_api_test.py
@@ -5,11 +5,11 @@ import requests
 class EchoApiTest(unittest.TestCase):
 
     def test_echo(self):
-        result = requests.get("https://nosecount.herokuapp.com/echo?p=value")
+        result = requests.get("http://localhost:8000/echo?p=value")
         self.assertEqual(result.status_code, 200)
         self.assertEqual(result.json()["p"], "value")
 
     def test_echo_without_p(self):
-        result = requests.get("https://nosecount.herokuapp.com/echo")
+        result = requests.get("http://localhost:8000/echo")
         self.assertEqual(result.status_code, 200)
         self.assertIsNone(result.json()["p"])
